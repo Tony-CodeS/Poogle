73ea26a1c4f90505d3183fad9556f2660e5ab36d
Tony <asolomon484@gmail.com>
2023-03-03 17:44:25 +0100

    Poogle

--- src/components/Community/community.js
+++ src/components/Community/community.js
@@ -3,7 +3,7 @@ import './community.css'
 import "aos/dist/aos.css"
 import {FaTelegramPlane} from 'react-icons/fa'
 import {IoLogoTwitter} from 'react-icons/io'
-import {SiDiscord} from 'react-icons/si'
+
 import {BsMedium} from 'react-icons/bs'
 
 
@@ -12,7 +12,7 @@ function community() {
     <div>
       <div className='communityDiv' id='community'>
         <h1 className='communityTitle' data-aos="zoom-in"> Become a Poogleans... </h1>
-        <p> One of the marvelous things about community is that it enables us
+        <p> One of the most important things about community is that it enables us
            to welcome and help people in a way we couldn't as individuals.</p>
         <ul className='comLists'>
           <li className='comList' data-aos="zoom-in"> <a href='/'target="_blank" className='comLink'> <FaTelegramPlane className='comIcon'/> </a></li>
